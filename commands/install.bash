079a08cc063449da22d4a5394e90c0af0e727fb1
naga <wizard.shankar@gmail.com>
2024-03-15 11:28:37 +0530

    added installation

--- commands/install.bash
+++ commands/install.bash
@@ -3,4 +3,3 @@
 composer global config --no-plugins allow-plugins.dealerdirect/phpcodesniffer-composer-installer true
 composer global require --no-interaction magento/magento-coding-standard magento-ecg/coding-standard
 
-composer global -- exec phpcs --config-set installed_paths ../../magento/magento-coding-standard/, ../../magento-ecg/coding-standard
